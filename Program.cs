1f5675bc15099a07f10d012527b115e42f288f46
Vlad-Alexandru Tănăsescu <vlad_tanasescu@outlook.com>
2016-08-16 12:50:54 +0100

    Progress Counters & Minor Tweaks

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace CrackerX
+namespace BruteForce
 {
     class BruteForce
     {
         private static string password;
         private static string result;
-        private static long permutations;
+        private static ulong permutations;
 
         public static ConsoleKeyInfo key;
 
         private static bool isMatched = false;
 
         private static int charactersToTestLength = 0;
-        private static long computedKeys = 0;
+        private static ulong computedKeys = 0;
 
         private static char[] charactersToTest =
         {
@@ -29,7 +29,8 @@ namespace CrackerX
 
         static void Main(string[] args)
         {
-            Console.WriteLine("\nPlease enter an alphanumeric password. This program will attempt to crack it.\n");
+            Console.Title = "[DEBUG] Progress counters have been disabled in the code due to performance issues.".ToUpper();
+            Console.WriteLine("\nPlease enter an alphanumeric passphrase. This program will attempt to crack it.\n");
             while (true)
             {
                 do
@@ -64,12 +65,13 @@ namespace CrackerX
 
             for (int i=1; i<=password.Length; i++)
                 {
-                permutations += (long)Math.Pow(charactersToTest.Length, i);
+                permutations += (ulong)Math.Pow(charactersToTest.Length, i);
                 }
 
             var timeStarted = DateTime.Now;
             Console.WriteLine("\nBrute Force Started - {0}", timeStarted.ToString("dd MMM yyyy, HH:mm:ss.fffffff"));
             Console.WriteLine("Total Permutations: {0}\n", permutations);
+            //  Console.WriteLine();
 
             charactersToTestLength = charactersToTest.Length;
 
@@ -81,7 +83,8 @@ namespace CrackerX
                 startBruteForce(estimatedPasswordLength);
             }
 
-            Console.WriteLine("Password Matched - {0}", DateTime.Now.ToString("dd.MM.yyyy, HH:mm:ss.fffffff"));
+            //  Console.WriteLine();
+            Console.WriteLine("Passphrase Matched - {0}", DateTime.Now.ToString("dd.MM.yyyy, HH:mm:ss.fffffff"));
             Console.WriteLine("Time Passed: {0}", DateTime.Now.Subtract(timeStarted));
             Console.WriteLine("Resolved Password: {0}", result);
             Console.WriteLine("Permutations Computed: {0}", computedKeys);
@@ -107,6 +110,9 @@ namespace CrackerX
             var nextCharPosition = currentCharPosition + 1;
             for (int i = 0; i < charactersToTestLength; i++)
             {
+                //  Console.Title = string.Format("Status: {0:##0.00}% Complete", ((decimal)computedKeys / permutations) * 100);
+                //  Console.SetCursorPo‌​sition(0, Console.CursorTop - 1);
+                //  Console.WriteLine("Status: {0:##0.00}% Complete", ((decimal)computedKeys / permutations) * 100);
                 keyChars[currentCharPosition] = charactersToTest[i];
 
                 if (currentCharPosition < indexOfLastChar)
@@ -128,6 +134,9 @@ namespace CrackerX
                     }
                 }
             }
+            //  Console.Title = string.Format("Status: 100% Complete");
+            //  Console.SetCursorPo‌​sition(0, Console.CursorTop - 1);
+            //  Console.WriteLine("Status: 100% Complete");
         }
     }
 }
